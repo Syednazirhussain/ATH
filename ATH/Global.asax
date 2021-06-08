487daf0c539fcb748c0e4319391410a880dd4f26
Nazir <syednazir13@gmail.com>
2021-06-08 12:17:28 +0800

    Add project files.

---
+++ ATH/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ATH.MvcApplication" Language="C#" %>
---
+++ ATH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ATH.Startup))]
+namespace ATH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
